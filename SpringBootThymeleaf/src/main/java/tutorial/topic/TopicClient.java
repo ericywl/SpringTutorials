40e739059155790a35150056365a016b802cde74
ericywl <midnight2903@gmail.com>
2017-11-06 05:33:20 +0800

    added expand button
    made it simpler to test

--- SpringBootThymeleaf/src/main/java/tutorial/topic/TopicClient.java
+++ SpringBootThymeleaf/src/main/java/tutorial/topic/TopicClient.java
@@ -8,23 +8,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class TopicClient {
+//    @GetMapping("/topics")
+//    public String listAllTopics(Model model) {
+//        // consumes REST from FirstSpringBootDatabase
+//        RestTemplate restTemplate = new RestTemplate();
+//        String getUrl = "http://localhost:5000/topics";
+//
+//        // parse JSON to List<Topic>
+//        ResponseEntity<List<Topic>> topicsResponse = restTemplate.exchange(getUrl, HttpMethod.GET,
+//                null, new ParameterizedTypeReference<List<Topic>>() {});
+//
+//        List<Topic> topicList = topicsResponse.getBody();
+//        model.addAttribute("topics", topicList);
+//
+//        return "topics";
+//    }
+
     @GetMapping("/topics")
-    public String listAllTopics(Model model) {
-        // consumes REST from FirstSpringBootDatabase
-        RestTemplate restTemplate = new RestTemplate();
-        String getUrl = "http://localhost:5000/topics";
+    public String getTopics(Model model) {
+        Topic example = new Topic("python", "Python", "Python description");
+        Topic example2 = new Topic("java", "Java", "Java description");
+        List<Topic> topicList = new ArrayList<>();
+        topicList.add(example);
+        topicList.add(example2);
 
-        // parse JSON to List<Topic>
-        ResponseEntity<List<Topic>> topicsResponse = restTemplate.exchange(getUrl, HttpMethod.GET,
-                null, new ParameterizedTypeReference<List<Topic>>() {});
-
-        List<Topic> topicList = topicsResponse.getBody();
         model.addAttribute("topics", topicList);
-
         return "topics";
     }
 }
